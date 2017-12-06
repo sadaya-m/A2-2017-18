3f8b733b26150ed7216817e61cd1f9d034ebb5b6
Joyce <Joyce@DESKTOP-BEJFRN0.hitronhub.home>
2017-12-05 21:22:21 -0600

    First commit

---
+++ src/SkaterClass.java
@@ -0,0 +1,27 @@
+/* **********************************************************
+ * Programmer:	Ma Pauline Sadaya
+ * Class:	CS30S
+ * 
+ * Assignment:	A2-2017-18
+ *
+ * Description:	skater class
+ * *************************************************************/
+ 
+ // import files here as needed
+ 
+ 
+ public class SkaterClass
+ {  // begin class
+ 	
+    // *********** class constants ***********
+
+
+    // ********** instance variable **********
+
+    // ********** constructors ***************
+
+    // ********** accessors ******************
+
+    // ********** mutators *******************
+ 
+ }  // end class
---
+++ src/SkaterClient.java
@@ -0,0 +1,59 @@
+// **********************************************************************
+//  Programmer:         Ma Pauline Sadaya
+//  Class:		CS30S
+//
+//  Assignment:         A2-2017-18
+//
+//  Description:	skater client
+//
+//
+//  Input:		describe any input from keyboard or file
+//
+//  Output:		describe the result of your program
+// ***********************************************************************
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class SkaterClient
+{  // begin class
+    public static void main(String args[])
+    {  // begin main
+    // ***** declaration of constants *****
+
+    // ***** declaration of variables *****
+
+    // ***** create objects *****
+
+
+    // ***** create input stream *****
+
+        //ConsoleReader console = new ConsoleReader(System.in);
+
+    // ***** Print Banner *****
+
+        System.out.println("**********************************");
+        System.out.println("NAME:        Ma. Pauline Sadaya");
+        System.out.println("Class:       CS30S");
+        System.out.println("Assignment:  A2-2017-18");
+        System.out.println("**********************************");
+
+    // ***** get input *****
+
+        // all input is gathered in this section
+        // remember to put ConsoleReader.class into the
+        // same folder.
+
+    // ***** processing *****
+
+
+    // ***** output *****
+
+        // all formatted ouput is printed in this section
+
+    // ***** closing message *****
+
+        System.out.println("end of processing");
+
+	}  // end main	
+}  // end class
